c56f5291eee7267a4557a3f10218faa01ecce730
TheSowut <sowutmail@gmail.com>
2024-02-10 23:57:43 +0200

    feat: initialize FE

--- server/app.ts
+++ server/app.ts
@@ -3,20 +3,27 @@ import express from "express";
 import redis from "./redis";
 
 const app = express();
+const cors = require('cors');
 const port = 8080;
+
+app.use(cors());
 
 app.get('/', (_, res) => {
     res.sendStatus(200);
 });
 
 app.get('/test', async (_, res) => {
-    await redis.set('key', Date.now());
-    const a = await redis.get('key');
-
     res.send({
         status: 200,
-        date: a
-    });
+        mesage: "asd"
+    })
+    // await redis.set('key', Date.now());
+    // const a = await redis.get('key');
+
+    // res.send({
+    //     status: 200,
+    //     date: a
+    // });
 });
 
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
